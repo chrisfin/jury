d903242ed537389db6431a6755e3214ecf366761
Chris Finlayson <chrisfinlayson@gmail.com>
2014-08-24 15:34:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Judgy::Application.config.secret_key_base = 'd086c5dd4e22b73d54b9cedec4825a6e0ec70421f8d95ac81f925b8676cc1087b78909dea3418227993134f6171a13775847f5affd752e6fb648f8e29ac43971'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Judgy::Application.config.session_store :cookie_store, key: '_judgy_session'
