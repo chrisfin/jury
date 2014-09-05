a6ebeb997fc4fdf23bd152621d6d47d280053d2c
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-05 17:19:43 -0400

    Style Devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,9 @@ class PagesController < ApplicationController
 		# Add User Email to MailChimp through API
 	  gb = Gibbon::API.new
 	  email = params[:user][:email]
+	  cookies[:email] = email
 	  list_id = '1482a5058b'
 	  gb.lists.subscribe({:id => list_id, :email => { "email" => email }, :double_optin => false})
-	    
 	 end
 
 
