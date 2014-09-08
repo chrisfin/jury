d6b9296faac8219eda0a5d73a18bf0a12baa66e1
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-08 12:56:35 -0400

    help

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Judgy
   class Application < Rails::Application
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag}
+    config.assets.precompile += ['pages.css.scss'] 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
