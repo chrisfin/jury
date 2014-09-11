92e2b399005e817ccb7b7cba9449d722e087d691
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-11 15:02:51 -0400

    Test resize image for facebook share

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,7 +36,11 @@ class ProjectsController < ApplicationController
 
 	def show
 		@project = Project.find(params[:id])
+    
+  #Metadata for social share buttons
     @title = "#{@project.project_name} - #{@project.tagline}"
+    @url = 
+    @image =
 
     if current_user
       @user = current_user.id
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	has_attached_file :image, styles: { thumb: '120x120', large: '300x400', twitter: '650x488' }
+	has_attached_file :image, styles: { thumb: '120x120', large: '300x400', twitter: '650x488', facebook: '500x250' }
   
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
