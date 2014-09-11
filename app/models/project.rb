13df5496e3fd48fc348bef1678e7c48e941737f2
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-11 16:55:41 -0400

    Test extant for facebook share

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-	has_attached_file :image, styles: { thumb: '120x120', large: '300x400', twitter: '650x488', facebook: '500x250' }
+	has_attached_file :image, styles: { thumb: '120x120', large: '300x400', twitter: '650x488', facebook: '500x250' }, 
+    :convert_options => {:facebook => "-gravity center -extent 500x250"}
   
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
