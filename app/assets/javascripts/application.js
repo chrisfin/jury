20be866ad160ac7e3388e911aab257bd0658a7ff
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-11 18:39:44 -0400

    Add Twitter share meta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,3 @@
 //= require bootstrap.min
 //= require social-share-button
 //= require_tree .
-
-$fbShare(url, title, descr, image, winWidth, winHeight).observe('click', function (event) {
-      var winTop = (screen.height / 2) - (winHeight / 2);
-      var winLeft = (screen.width / 2) - (winWidth / 2);
-      window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-  })
