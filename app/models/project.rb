4a8c109c33be2f052050ca128b8861ebe3b3da91
Chris Finlayson <chrisfinlayson@gmail.com>
2014-09-29 23:35:14 -0400

    Change image rendering on project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	has_attached_file :image, styles: { thumb: '120x120', large: '300x400', twitter: '650x488', facebook: '1000x500' }, 
+	has_attached_file :image, styles: { thumb: '120x120', large: '700x450', twitter: '1000x500', facebook: '1000x500' }, 
     :convert_options => {:facebook => "-gravity center -extent 1000x500^"}
   
   validates_attachment :image, presence: true,
